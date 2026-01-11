f46e265cf4553ba968c8ef088ce415a032886d78
Abhrajit Debnath <137435505+Abhrajit-debnath@users.noreply.github.com>
2026-01-11 16:24:33 +0530

    add root route middleware check

--- proxy.ts
+++ proxy.ts
@@ -5,12 +5,22 @@ export function proxy(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
   const pathname = req.nextUrl.pathname;
 
-  if (!token && pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/auth/signin", req.url));
+    if (pathname === "/") {
+    return NextResponse.redirect(
+      new URL(token ? "/dashboard/home" : "/auth/signin", req.url)
+    );
   }
 
+  if (!token && pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(
+      new URL("/auth/signin", req.url)
+    );
+  }
+  
   if (token && pathname === "/auth/signin") {
-    return NextResponse.redirect(new URL("/dashboard/home", req.url));
+    return NextResponse.redirect(
+      new URL("/dashboard/home", req.url)
+    );
   }
 
   return NextResponse.next();
