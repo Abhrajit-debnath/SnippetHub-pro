f244d82eacb8fee6d86a5c91389c0ebc2e59b0a9
Abhrajit Debnath <debnath.abhrajit2005@gmail.com>
2026-04-21 08:36:45 +0530

    style: fix button color

--- app/Layout/components/sidebar/sidebar.tsx
+++ app/Layout/components/sidebar/sidebar.tsx
@@ -1,51 +1,7 @@
-// "use client";
-
-// import { Plus } from "lucide-react";
-// import React, { useEffect, useState } from "react";
-// import SidebarMenus from "@/app/Layout/components/snippet/sidebar-menus";
-
-// type SidebarProps = {
-//   toggleSidebar: boolean;
-//   setToggleSidebar: React.Dispatch<React.SetStateAction<boolean>>;
-// };
-
-// const Sidebar = ({ toggleSidebar, setToggleSidebar }: SidebarProps) => {
-//   const [mounted, setMounted] = useState(false);
-
-//   useEffect(() => {
-//     setMounted(true);
-//   }, []);
-
-//   if (!mounted) return null;
-
-//   return (
-//     <aside
-//       className={`fixed top-0 left-0 z-20 h-screen w-64 lg:hidden
-//        bg-sidebarBg
-//         text-white
-//         transform transition-transform duration-300 ease-in-out
-//         ${toggleSidebar ? "translate-x-0" : "-translate-x-full"}
-//       `}
-//     >
-//       <button
-//         type="button"
-//         className="rotate-45 fixed right-5 top-4"
-//         onClick={() => setToggleSidebar(false)}
-//       >
-//         <Plus className="text-white" />
-//       </button>
-
-//       <SidebarMenus setToggleSidebar={setToggleSidebar}/>
-//     </aside>
-//   );
-// };
-
-// export default Sidebar;
-
-
 "use client";
 
-import { Sheet, SheetContent } from "@/components/ui/sheet";
+import { Plus } from "lucide-react";
+import React, { useEffect, useState } from "react";
 import SidebarMenus from "@/app/Layout/components/snippet/sidebar-menus";
 
 type SidebarProps = {
@@ -54,16 +10,35 @@ type SidebarProps = {
 };
 
 const Sidebar = ({ toggleSidebar, setToggleSidebar }: SidebarProps) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
-    <Sheet open={toggleSidebar} onOpenChange={setToggleSidebar}>
-      <SheetContent
-        side="left"
-        className="bg-sidebarBg p-0 w-64 border-zinc-700"
+    <aside
+      className={`fixed top-0 left-0 z-20 h-screen w-64 lg:hidden
+       bg-sidebarBg
+        text-white
+        transform transition-transform duration-300 ease-in-out
+        ${toggleSidebar ? "translate-x-0" : "-translate-x-full"}
+      `}
+    >
+      <button
+        type="button"
+        className="rotate-45 fixed right-5 top-4"
+        onClick={() => setToggleSidebar(false)}
       >
-        <SidebarMenus setToggleSidebar={setToggleSidebar} />
-      </SheetContent>
-    </Sheet>
+        <Plus className="text-white" />
+      </button>
+
+      <SidebarMenus setToggleSidebar={setToggleSidebar}/>
+    </aside>
   );
 };
 
 export default Sidebar;
+
